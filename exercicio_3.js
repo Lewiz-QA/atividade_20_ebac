b6e99d77fb68a54d6608e03308fde3f5e301e6fd
Luiz Menon <103550431+Lewiz-QA@users.noreply.github.com>
2023-01-12 22:35:44 -0300

    Exercícios

---
+++ exercicio_3.js
@@ -0,0 +1,9 @@
+somarMultiplos(1000);
+function somarMultiplos(limite) {
+    let resultado = 0
+    for (let i = 0; i < limite; i++) {
+        if (i % 5 === 0 || i % 7 === 0)
+            resultado += i;
+    }
+    console.log("Soma de todos os múltiplos de 5 e 7 abaixo de 1000: " + resultado)
+}
